f48a9d48bbad2a985e3f7168cb86aa0e50a60734
vagabond0091 <endayajerry11@gmail.com>
2021-08-08 10:18:15 +0800

    change main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
     <!-- Scripts -->
   
-    <link href="{{ secure_asset('storage/scss/main.css') }}" rel="stylesheet">
+    <link href="storage/scss/main.css" rel="stylesheet">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
