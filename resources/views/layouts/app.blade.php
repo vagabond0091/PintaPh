ce978b0084b2fbb2ba7eab0163b2e6866b4e7c15
vagabond0091 <endayajerry11@gmail.com>
2021-08-08 10:16:54 +0800

    change main.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <link href="{{ asset('storage/scss/main.css') }}" rel="stylesheet">
+   
+    <link href="{{ secure_asset('storage/scss/main.css') }}" rel="stylesheet">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+   
 </head>
 <body>
     <div id="app">
